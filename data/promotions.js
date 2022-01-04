a8933bd88e5faea88b7a5014a3bdf65308cc7e9e
Gon Freecs <mastergdeo@gmail.com>
2022-01-03 21:40:37 -0800

    Updates

---
+++ data/promotions.js
@@ -0,0 +1,6 @@
+const promotions = [
+    "https://t4.ftcdn.net/jpg/02/62/03/53/360_F_262035364_gGi8uJsPl9uljis8C6oxI0w6AM7MKDLq.jpg",
+    "https://cdn2.vectorstock.com/i/1000x1000/68/91/promotion-banner-design-vector-28116891.jpg"
+]
+
+export default promotions;
