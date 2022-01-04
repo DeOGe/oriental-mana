78e43075bb14e7803ded02833e41d43f57eebf99
Gon Freecs <mastergdeo@gmail.com>
2022-01-03 21:50:43 -0800

    Updates

---
+++ data/products.js
@@ -0,0 +1,88 @@
+const products = [
+    {
+        "id": "1",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Cheese Mini Soy Roll",
+        "price": "320.00",
+        "image": "./images/cheese_mini_soy_roll.jpg"
+    },
+    {
+        "id": "2",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Melona",
+        "price": "200.00",
+        "image": "./images/melona.jpg"
+    },
+    {
+        "id": "3",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Soybean Slice",
+        "price": "245.00",
+        "image": "./images/soybean_slice.jpg"
+    },
+    {
+        "id": "4",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Vegan Fried Squid Ring",
+        "price": "180.00",
+        "image": "./images/vegan_fried_squid_ring.jpg"
+    },
+    {
+        "id": "5",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Cheese Mini Soy Roll",
+        "price": "320.00",
+        "image": "./images/cheese_mini_soy_roll.jpg"
+    },
+    {
+        "id": "6",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Melona",
+        "price": "200.00",
+        "image": "./images/melona.jpg"
+    },
+    {
+        "id": "7",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Soybean Slice",
+        "price": "245.00",
+        "image": "./images/soybean_slice.jpg"
+    },
+    {
+        "id": "8",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Vegan Fried Squid Ring",
+        "price": "180.00",
+        "image": "./images/vegan_fried_squid_ring.jpg"
+    },
+    {
+        "id": "9",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Cheese Mini Soy Roll",
+        "price": "320.00",
+        "image": "./images/cheese_mini_soy_roll.jpg"
+    },
+    {
+        "id": "10",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Melona",
+        "price": "200.00",
+        "image": "./images/melona.jpg"
+    },
+    {
+        "id": "11",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Soybean Slice",
+        "price": "245.00",
+        "image": "./images/soybean_slice.jpg"
+    },
+    {
+        "id": "12",
+        "createdAt": "2021-06-22T13:57:41.039Z",
+        "name": "Vegan Fried Squid Ring",
+        "price": "180.00",
+        "image": "./images/vegan_fried_squid_ring.jpg"
+    }
+]
+
+export default products;
